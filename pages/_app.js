b922d5afbc4a5e7b46d645a3196ced08ef52ac61
therealozp <anhkhang.le0910@gmail.com>
2023-08-12 13:32:53 -0400

    changed to pages paradigm

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import { FileProvider } from '../context/FileProvider';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'next-auth/client';
+import { CacheProvider } from '@chakra-ui/react';
 import Layout from '@/components/layout/layout';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider session={pageProps.session}>
 			<FileProvider>
-				<ChakraProvider>
-					<Layout>
-						<Component {...pageProps} />
-					</Layout>
-				</ChakraProvider>
+				<CacheProvider>
+					<ChakraProvider>
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					</ChakraProvider>
+				</CacheProvider>
 			</FileProvider>
 		</Provider>
 	);
